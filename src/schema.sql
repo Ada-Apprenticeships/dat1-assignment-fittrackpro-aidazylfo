3bdbb0eb40e5ef418b99371def40da0a42b5ea1f
aidazylfo <aida.zylfo@ada.ac.uk>
2025-01-30 16:15:48 +0000

    hi

--- src/schema.sql
+++ src/schema.sql
@@ -9,11 +9,13 @@ DROP TABLE IF EXISTS locations;
 CREATE TABLE locations (
     location_id INTEGER PRIMARY KEY, 
     name TEXT,
-    address INTEGER,
+    address TEXT,
     phone_number INTEGER,
-    email INTEGER, 
+    email TEXT, 
     opening_hours TEXT
 );
+
+DROP TABLE IF EXISTS members;
 
 CREATE TABLE members (
     member_id INTEGER PRIMARY KEY,
@@ -27,16 +29,20 @@ CREATE TABLE members (
     emergency_contact_phone INTEGER
 );
     
+DROP TABLE IF EXISTS staff;
+
 CREATE TABLE staff (
     staff_id INTEGER PRIMARY KEY,
     first_name TEXT,
     last_name TEXT,	
     email TEXT,	
-    phone_number TEXT,
+    phone_number INTEGER,
     position TEXT,	
     hire_date TEXT,	
-    location_id INTEGER REFERENCE
+    location_id TEXT REFERENCE locations (location_id)
 );
+
+DROP TABLE IF EXISTS equipment;
 
 CREATE TABLE equipment (
    equipment_id	INTEGER PRIMARY KEY,
@@ -45,8 +51,10 @@ CREATE TABLE equipment (
    purchase_date TEXT,
    last_maintenance_date TEXT,	
    next_maintenance_date TEXT,	
-   location_id INTEGER REFERENCE
+   location_id TEXT REFERENCE locations (location_id)
 );
+
+DROP TABLE IF EXISTS classes;
 
 CREATE TABLE classes (
     class_id INTEGER PRIMARY KEY,
@@ -54,50 +62,61 @@ CREATE TABLE classes (
     description TEXT,
     capacity TEXT,
     duration TEXT,
+    location_id TEXT
 );
+
+DROP TABLE IF EXISTS class_attendance;
 
 CREATE TABLE class_attendance (
     class_attendance_id	INTEGER PRIMARY KEY,
-    schedule_id	TEXT, REFERENCE
-    member_id TEXT, REFERENCE
+    schedule_id	TEXT, REFERENCE class_attendance (schedule_id)
+    member_id TEXT, REFERENCE members (member_id)
     attendance_status TEXT 
 );
 
+DROP TABLE IF EXISTS payments;
+
 CREATE TABLE payments (
     payment_id INTEGER PRIMARY KEY,
-    member_id TEXT, REFERENCE
-    amount TEXT,
+    member_id TEXT, REFERENCE  (member_id)
+    amount INTEGER,
     payment_date TEXT,	
     payment_method TEXT,
     payment_type TEXT
 );
 
+DROP TABLE IF EXISTS personal_training_sessions;
+
 CREATE TABLE personal_training_sessions (
     session_id INTEGER PRIMARY KEY,
-    member_id TEXT, REFERENCE
-    staff_id TEXT, REFERENCE
+    member_id TEXT, REFERENCE members (member_id)
+    staff_id TEXT, REFERENCE staff (staff_id)
     session_date TEXT,
     start_time TEXT,
     end_time TEXT,
     notes TEXT
 );
 
+DROP TABLE IF EXISTS member_health_metrics;
+
 CREATE TABLE member_health_metrics (
     metric_id INTEGER PRIMARY KEY,
-    member_id TEXT, REFERENCE
+    member_id TEXT, REFERENCE members (member_id)
     measurement_date TEXT,
     weight TEXT,
     body_fat_percentage TEXT,
     muscle_mass TEXT,
-    bmi TEXTn
+    bmi TEXT
 );
+
+DROP TABLE IF EXISTS equipment_maintenance_log;
 
 CREATE TABLE equipment_maintenance_log (
     log_id INTEGER PRIMARY KEY AUTOINCREMENT, -- means you dont have to apply a value to the integer the database will do it 
     equipment_id TEXT,
     maintenance_date TEXT,
     description TEXT,
-    staff_id TEXT REFERENCE
+    staff_id TEXT REFERENCE staff (staff_id)
 );
 
 
