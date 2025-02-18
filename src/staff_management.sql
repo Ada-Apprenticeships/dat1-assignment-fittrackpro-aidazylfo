b0c67fa297a19cf3930c53ad7de2ca13a7ebf560
aidazylfo <aida.zylfo@ada.ac.uk>
2025-02-18 16:22:49 +0000

    staff management

--- src/staff_management.sql
+++ src/staff_management.sql
@@ -14,24 +14,13 @@ ORDER BY position;
 
 -- 2. Find trainers with one or more personal training session in the next 30 days
 -- TODO: Write a query to find trainers with one or more personal training session in the next 30 days
-SELECT staff_id,
-       first_name 
-,position
-FROM staff 
-WHERE position = 'Trainer'; --use class schedulle table 
-
-
-
-
-
-
-
-
-SELECT staff_id, 
-       first_name || ' ' || last_name AS trainer_name, 
-       COUNT(   ) AS session_count
-FROM trainers
-JOIN personal_training_sessions pt ON t.trainer_id = pt.trainer_id
-WHERE pt.session_date BETWEEN CURRENT_DATE AND DATE(CURRENT_DATE, '+30 day')
-GROUP BY t.trainer_id
+-- 2. Find trainers with one or more personal training session in the next 30 days
+SELECT s.staff_id, 
+       s.first_name || ' ' || s.last_name AS trainer_name, 
+       COUNT(pt.session_id) AS session_count
+FROM staff s
+JOIN personal_training_sessions pt ON s.staff_id = pt.staff_id
+WHERE s.position = 'Trainer' 
+  AND pt.session_date BETWEEN CURRENT_DATE AND DATE(CURRENT_DATE, '+30 day')
+GROUP BY s.staff_id
 HAVING COUNT(pt.session_id) > 0;
